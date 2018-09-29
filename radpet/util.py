b15bb948b43096c8620c743ef5f466b42370c918
Radoslav Petkov <radoslav.petkov@sirma.bg>
2018-09-29 11:05:50 +0300

    initial eda

---
+++ radpet/util.py
@@ -0,0 +1,5 @@
+NAME = 'ifm_analysis_sensordata_for_datathon_final.machine_name'
+TYPE = 'ifm_analysis_sensordata_for_datathon_final.sensor_type'
+VALUE = 'ifm_analysis_sensordata_for_datathon_final.realvalue'
+
+DATE = 'ifm_analysis_sensordata_for_datathon_final.date_measurement'
