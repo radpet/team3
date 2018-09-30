9b88cad48d929e315f7722d1c0e0d8a98af7ed64
Radoslav Petkov <radoslav.petkov@sirma.bg>
2018-09-30 12:55:03 +0300

    some plotting

--- radpet/util.py
+++ radpet/util.py
@@ -3,3 +3,9 @@ TYPE = 'ifm_analysis_sensordata_for_datathon_final.sensor_type'
 VALUE = 'ifm_analysis_sensordata_for_datathon_final.realvalue'
 
 DATE = 'ifm_analysis_sensordata_for_datathon_final.date_measurement'
+
+sensors = ['lifting_motor_a_max', 'lifting_gear_a_max', 'idle_wheel_a_max',
+       'drive_wheel_a_max', 'drive_motor_a_max', 'drive_gear_a_max',
+       'drive_wheel_V_eff', 'lifting_motor_V_eff', 'idle_wheel_V_eff',
+       'lifting_gear_V_eff', 'drive_motor_V_eff', 'drive_gear_V_eff']
+
